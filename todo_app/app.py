36739fd6b52bd6311a2afebd6d995ca806051cb2
Mark Dos Santos <mark.santos@stericycle.com>
2022-01-13 01:35:09 +0000

    Completed first exercise

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,11 +1,18 @@
-from flask import Flask
-
+from flask import Flask, render_template
+from flask import request
+from flask import redirect
+from todo_app.data.session_items import get_items
+from todo_app.data.session_items import add_item
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config())
 
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-    return 'Hello World!'
+    if request.method == 'POST':
+        add_item(request.form.get('title'))
+        return redirect('/')
+    else:
+        return render_template('index.html', items=get_items())
